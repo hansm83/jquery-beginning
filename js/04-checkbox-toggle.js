09854359ae677dcf009233f9dffec5897481f147
hansm83 <aioie@naver.com>
2017-01-19 09:47:16 +0900

    04-checkbox-toggle add

---
+++ js/04-checkbox-toggle.js
@@ -0,0 +1,13 @@
+/*! 04-slider.js */
+
+(function(global, $){
+
+'use strict';
+
+	// checkbox toggle
+	$('.ui_check').on('click', function() {
+		$(this).parent().toggleClass('on');;
+	});
+
+})(this, jQuery);
+
