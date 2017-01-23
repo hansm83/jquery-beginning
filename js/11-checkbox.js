31e1378b4d5aac07f90026206a4f40e00751fbbe
hansm83 <aioie@naver.com>
2017-01-23 13:55:06 +0900

    11-checkbox add

---
+++ js/11-checkbox.js
@@ -0,0 +1,21 @@
+/*! 04-slider.js */
+
+(function(global, $){
+
+'use strict';
+
+	// Checkbox
+	var checkBox = $('.checkbox_wrap');
+	var addClassCheckBox = function($input) {
+		if ($input.prop('checked')) {
+		  $input.parent().addClass('checked');
+		} else {
+		  $input.parent().removeClass('checked');
+		}
+	};
+	checkBox.on('change', 'input', function() {
+		addClassCheckBox($(this));
+	})
+
+})(this, jQuery);
+
